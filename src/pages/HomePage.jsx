2c1cf3f2898d510215ba0992d9e0a41d3791bed3
nine-chairs <havlik9@gmail.com>
2023-12-13 17:01:57 +0100

    updated layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,19 +10,17 @@ const HomePage = () => {
         <h1 className="header-title">COSALUX</h1>
       </header>
 
-
-
       <div className="container-wrapper">
-        <div className="ContainerOne">
+
+        <div className="containerOne">
           <text className='containerOneText'>
             DARING TO BE DIFFERENT.
           </text>
         </div>
 
-        <div className="ContainerTwo">
-          <div className='ContainerTwoContent'>
-
-            <div className='ContentLeft'>
+        <div className="containerTwo">
+          <div className='containerTwoContent'>
+            <div className='contentLeft'>
               <div className='contentLeftTitleContainer'>
                 <text className='contentLeftTitle'>
                   SAG HALLO!<br />
@@ -35,7 +33,6 @@ const HomePage = () => {
                 </text>
               </div>
             </div>
-
             <div className='contentRight'>
               <QRCode />
               <div className='buttonContainer'>
@@ -43,14 +40,12 @@ const HomePage = () => {
                   Button Text
                 </button>
               </div>
-
             </div>
-
           </div>
         </div>
 
-        <div className="ContainerThree">
-          <div className='ContainerThreeContent'>
+        <div className="containerThree">
+          <div className='containerThreeContent'>
 
             <div className='contentTop'>
               <div className='contentTopTitleContainer'>
@@ -64,17 +59,42 @@ const HomePage = () => {
                   COSALUX ist eine inhabergeführte Agentur für digitale Kommunikation und Design. Seit 2009 konzipieren und entwickeln wir aus Leidenschaft kreative und innovative Lösungen für unterschiedliche Branchen und Industrien.<br /> <br /> Wir transformieren „Zukunft” in Design und wissen, dass Digital ein anderes Wort für Emotion ist und Technologie für Neugier steht. Wir geben Produkten die Form zu sprechen und regen die Intelligenz zu interagieren an. Wir verstehen, dass Anders besser ist.<br /> <br /> We are DARING TO BE DIFFERENT.
                 </text>
               </div>
+            </div>
 
+            <div className='contentMiddle'>
+              <div className='contentMiddleTop'>
+                <text className='contentMiddleTopText'>ANLEITUNG</text>
+              </div>
+              <div className='contentMiddleBottom'>
+                <div className='stepContainer'>
+                  <text className='stepNumber'>1</text>
+                  <text className='stepText'>QR -Code scannen<br /> oder Link klicken.</text>
+                </div>
+                <div className='stepContainer'>
+                  <text className='stepNumber'>2</text>
+                  <text className='stepText'>WhatsApp öffnet sich</text>
+                </div>
+                <div className='stepContainer'>
+                  <text className='stepNumber'>3</text>
+                  <text className='stepText'>Nachricht abschicken.</text>
+                </div>
 
+              </div>
 
             </div>
 
-            <div className='ContentMiddle'>
-              <text>Mid Content</text>
-            </div>
 
-            <div className='ContentBottom'>
-              <text>Bottom Content</text>
+            <div className='contentTop'>
+              <div className='contentTopTitleContainer'>
+                <text className='contentTopTitle'>
+                  ÜBER UNS
+                </text>
+              </div>
+              <div className='contentTopTextContainer'>
+                <text className='contentTopText'>
+                  DARING TO BE DIFFERENT. <br />  <br />Das ist, woran wir glauben. Das ist was uns antreibt. Wir stechen heraus. Wir sind immer in Bewegung. Mit der Zeit und gegen den Strom. Wir glauben daran, dass Grenzen zum Überschreiten existieren. Wir denken, dass Mut und Herz neue Welten schaffen. Wir wissen, dass die Zukunft durch Träume geformt wird. Wir hoffen, etwas Bedeutendes zu hinterlassen.
+                </text>
+              </div>
             </div>
 
 
@@ -83,8 +103,11 @@ const HomePage = () => {
       </div>
 
       <footer className="footer">
-        footer
-        {/* Footer Content */}
+        © COSALUX GmbH 2023<br />
+        E info@cosalux.com<br />
+        T +49 (0)69 – 860 07 22 – 660<br />
+        F +49 (0)69 – 860 07 22 – 610
+
       </footer>
     </div >
   );
@@ -94,10 +117,3 @@ export default HomePage;
 
 
 
-// https://apps.apple.com/app/whatsapp/id310633997
-
-// https://play.google.com/store/apps/details?id=com.whatsapp
-
-
-
-// http://tiny.cc/li3ivz
