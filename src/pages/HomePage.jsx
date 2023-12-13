158871b3ddf18574f601bb42f8f58cfcf2c5d494
nine-chairs <havlik9@gmail.com>
2023-12-13 15:59:25 +0100

    updated styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,35 +14,59 @@ const HomePage = () => {
 
       <div className="container-wrapper">
         <div className="ContainerOne">
-          <text className='ContainerOneText'>
-            DARING TO BE DIFFERENT
+          <text className='containerOneText'>
+            DARING TO BE DIFFERENT.
           </text>
         </div>
 
         <div className="ContainerTwo">
           <div className='ContainerTwoContent'>
+
             <div className='ContentLeft'>
-              <text>SAG HALLO! WIR FREUEN UNS AUF DEINE ANFRAGE!</text>
-              <br />
-              <text>
-                Unser Horizont ist keine Linie, sondern 360 Grad – damit aus Projekten Welten werden. Wir wagen uns auf Wege, bevor sie existieren. Durch VR/AR-App Programmierung, UX/UI-Design Projekten, innovative Konzepte und interaktive Ausstellungsdesigns haben wir unsere Expertise für hochkarätige Kunden eingebracht.
-              </text>
+              <div className='contentLeftTitleContainer'>
+                <text className='contentLeftTitle'>
+                  SAG HALLO!<br />
+                  WIR FREUEN UNS AUF DEINE ANFRAGE!
+                </text>
+              </div>
+              <div className='contentLeftTextContainer'>
+                <text className='contentLeftText'>
+                  Unser Horizont ist keine Linie, sondern 360 Grad – damit aus Projekten Welten werden. Wir wagen uns auf Wege, bevor sie existieren. Durch VR/AR-App Programmierung, UX/UI-Design Projekten, innovative Konzepte und interaktive Ausstellungsdesigns haben wir unsere Expertise für hochkarätige Kunden eingebracht.
+                </text>
+              </div>
             </div>
-            <div className='ContentRight'>
+
+            <div className='contentRight'>
               <QRCode />
-              <button>
-                button text
-              </button>
+              <div className='buttonContainer'>
+                <button>
+                  Button Text
+                </button>
+              </div>
+
             </div>
+
           </div>
         </div>
 
         <div className="ContainerThree">
           <div className='ContainerThreeContent'>
-            <div className='ContentTop'>
-              <text>AGENTUR FÜR DIGITALE KOMMUNIKATION UND DESIGN</text>
-              <br />
-              <text>Top Content</text>
+
+            <div className='contentTop'>
+              <div className='contentTopTitleContainer'>
+                <text className='contentTopTitle'>
+                  AGENTUR FÜR DIGITALE<br />
+                  KOMMUNIKATION UND DESIGN
+                </text>
+              </div>
+              <div className='contentTopTextContainer'>
+                <text className='contentTopText'>
+                  COSALUX ist eine inhabergeführte Agentur für digitale Kommunikation und Design. Seit 2009 konzipieren und entwickeln wir aus Leidenschaft kreative und innovative Lösungen für unterschiedliche Branchen und Industrien.<br /> <br /> Wir transformieren „Zukunft” in Design und wissen, dass Digital ein anderes Wort für Emotion ist und Technologie für Neugier steht. Wir geben Produkten die Form zu sprechen und regen die Intelligenz zu interagieren an. Wir verstehen, dass Anders besser ist.<br /> <br /> We are DARING TO BE DIFFERENT.
+                </text>
+              </div>
+
+
+
             </div>
 
             <div className='ContentMiddle'>
@@ -62,7 +86,7 @@ const HomePage = () => {
         footer
         {/* Footer Content */}
       </footer>
-    </div>
+    </div >
   );
 }
 
